ac9e97dfee1b6613782a5d45b8bcd173bf091515
thanh23697 <thanhntt@hybrid-technologies.vn>
2022-12-02 14:24:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Header from './layout/header';
 import Footer from './layout/footer';
 import ProductItem from './components/product-item';
-import Child1 from "./components/child1";
-import Child2 from "./components/child2";
 
 function App() {
   const [data, setData] = useState([
@@ -11,47 +9,45 @@ function App() {
       name: "Cá hú tươi 500g",
       img: "https://product.hstatic.net/200000459373/product/tai_xuong__9__e20bf99465594802829fb0178e95d85d_large.jpg",
       price: "32,800",
-      vendors: "OsiFood"
+      vendors: "OsiFood",
+      quantity: 1
     },
     {
       name: "Canh bí đao thịt xay 300g",
       img: "https://product.hstatic.net/200000459373/product/untitled_design__5__f8e03b03d3e84f33858dd2058851d090_large.jpg",
       price: "30,000",
-      vendors: "OsiFood"
+      vendors: "OsiFood",
+      quantity: 3
     },
     {
       name: "Canh bí đỏ tròn thịt xay 300g",
       img: "https://product.hstatic.net/200000459373/product/untitled_design__4__a60e0a1d092d45ec8536d6f1d7e36973_large.jpg",
       price: "29,000",
-      vendors: "OsiFood"
+      vendors: "OsiFood",
+      quantity: 4
     },
     {
       name: "Canh bí đỏ tròn vỉ 300g",
       img: "https://product.hstatic.net/200000459373/product/untitled_design__3__96ad1587d67b4504b64caef1f4fd7750_large.jpg",
       price: "16,000",
-      vendors: "OsiFood"
+      vendors: "OsiFood",
+      quantity: 0
     },
     {
       name: "Cánh gà 500g- Sanha",
       img: "https://product.hstatic.net/200000459373/product/untitled_design__14__e585692a9ac945c1b0b8e17baa4a99d1_large.jpg",
       price: "41,000",
-      vendors: "Sanha"
+      vendors: "Sanha",
+      quantity: 2
     },
     {
       name: "Sữa lúa mạch Milo lốc 4 hộp 180ml",
       img: "https://product.hstatic.net/200000459373/product/untitled_design__9__c587ee8abbab4a8a967ee69e348cca8c_large.jpg",
       price: "30,000",
-      vendors: "Nestle"
+      vendors: "Nestle",
+      quantity: 10
     }
   ]);
-
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount(count + 1);
-  }
-
-
-  const [message,setMessage]=useState('empty');
 
   return (
     <div className="App">
@@ -68,14 +64,14 @@ function App() {
                   data.map((el, index) => <ProductItem data={el} key={index} />)
                 }
               </div>
-              {/* <p>Count: {count}</p> */}
+              <div className="section__btnMore">
+                <a href="/">Xem thêm sản phẩm</a>
+              </div>
             </div>
           </div>
         </div>
       </main>
-      <Footer handleClick={handleClick}/>
-      {/* <Child1 setMessage={setMessage} />
-      <Child2 selectedMessage = {message}/> */}
+      <Footer />
     </div>
   );
 }
--- src/components/product-item.js
+++ src/components/product-item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProductItem(props) {
   const {data= {
@@ -8,6 +8,27 @@ function ProductItem(props) {
     vendors: ""
   }} = props;
 
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const addCartClick = () => {
+    setShow(true);
+    setValue(value + 1);
+  }
+  const plusValue = () => {
+    if(value < data.quantity) {
+      setValue(value + 1);
+    }
+  }
+  const minusValue = () => {
+    if (value >= 1) {
+      setValue(value - 1);
+    } 
+    if (value < 2) {
+      setShow(false);
+    }
+  }
+
   return (
     <div className='productItem'>
       <div className='productItem__inner'>
@@ -15,12 +36,25 @@ function ProductItem(props) {
           <a href='/products/cahu'>
             <div className='productItem__img__inner' style={{ backgroundImage: `url(${data.img})` }}></div>
           </a>
+          {data.quantity <= 0 && <div className="pro-soldout"><span>Tạm hết hàng</span></div>}
         </div>
         <div className='productItem__detail'>
           <p className='productItem__detail__vendor'><a href='/colections/osifood'>{data.vendors}</a></p>
           <h3 className='productItem__detail__name'><a href='/products/cahu'>{data.name}</a></h3>
           <p className='productItem__detail__price'>{data.price}₫</p>
-          <a href='/' className='productItem__detail__addcart'><i className="fal fa-shopping-bag"></i><span>Chọn mua</span></a>
+          <div className="productItem__detail__action">
+            {data.quantity > 0 && <div className='productItem__detail__addcart' onClick={addCartClick}><i className="fal fa-shopping-bag"></i><span>Chọn mua</span></div>}
+            {data.quantity <= 0 && <div className='productItem__detail__addcart soldout'><span>Tạm hết hàng</span></div> }
+            { show && 
+              <div className="productItem__detail__quantity">
+                <div className="boxqty">
+                  <button onClick={minusValue}><i className="fal fa-minus"></i></button>
+                  <input type="text" value={value} onChange={e => setValue(e.target.value)} className="proloop-qtyvalue" />
+                  <button onClick={plusValue} className={`${value < data.quantity ? '' : 'disable'}`}><i className="fal fa-plus"></i></button>
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
     </div>
